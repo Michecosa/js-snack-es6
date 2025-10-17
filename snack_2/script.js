7a68e6945a0e017ac3e173829442e066c3b6a561
Michela <micheladellagatta1@gmail.com>
2025-10-17 15:03:53 +0200

    Add function to generate a random integer between a given range

--- snack_2/script.js
+++ snack_2/script.js
@@ -6,6 +6,10 @@ Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
 
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 const nomiSquadre = ["Napoli","Atlanta","Bologna","Cagliari","Corno","Cremonese","Fiorentina","Genoa","Inter","Juventus","Lazio","Lecce","Milan","Parma","Pisa","Roma","Sassuolo","Torino","Udinese","Verona"];
 
 const squadre = [];
